6fe998ad5be67c13a2886dcb3e897da821532d57
ces0712 <cesaraugusto0712@gmail.com>
2026-02-21 22:20:24 -0300

    fix(podman): podman image creation errors

--- hosts/forgejo-pi/packages.nix
+++ hosts/forgejo-pi/packages.nix
@@ -1,24 +1,17 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
-    # system utilities
     vim
     htop
     tmux
     git
-    # network
     curl
     dnsutils
     traceroute
     tailscale
-    # disk utilities
     parted
     smartmontools
-    # nix tools
-    nix-tree
-    nvd
-    # db
-    sqlite # used by forgejo-wal and backup
-    restic # used by backup
-    rclone # used by backup
+    sqlite
+    restic
+    rclone
   ];
 }
